f021f0c9e0b36d06cc95e9307e56fe7f95a0e762
Italo Stuardo Cortez Silva <ingeniero.stuardo@gmail.com>
2021-04-29 05:01:03 -0600

    building the basic functionality for react-router in App component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <Dashboard></Dashboard>
-      <Login />
-      <Error />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Dashboard></Dashboard>
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route path='*'>
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
