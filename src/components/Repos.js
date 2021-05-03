7bd806f6ed97e3e24778738256d9503835ef7399
Italo Stuardo Cortez Silva <ingeniero.stuardo@gmail.com>
2021-05-03 05:50:23 -0600

    building first example pie and pie chart in Pie3D component

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -5,7 +5,34 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 import { useGlobalContext } from '../context/context';
 
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const { repos } = useGlobalContext();
+  let languages = repos.reduce((total, { language }) => {
+    if (!language) return total;
+    if (!total[language]) {
+      total[language] = { label: language, value: 1 };
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+      };
+    }
+    return total;
+  }, {});
+  console.log(languages);
+  languages = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5);
+
+  return (
+    <section className='section'>
+      <Wrapper className='section-center'>
+        {/* <ExampleChart data={chartData} /> */}
+        <Pie3D data={languages} />
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
